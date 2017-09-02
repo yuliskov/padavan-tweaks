810fbdacb881a6ea01da30273fa39e971027cb1d
Yuriy Liskov <firsthash@gmail.com>
2017-09-02 23:25:02 +0300

    my_scripts

--- my_scripts/redirect.sh
+++ my_scripts/redirect.sh
@@ -20,20 +20,19 @@ func_toggle_redirect()
     RULE_PATTERN="iptables -t nat %s PREROUTING --src %s ! --dest my.router -p tcp --dport 80 -j REDIRECT --to-ports $SVC_PORT"
 
     SRC_IP_MSG=""
-    SRC_IP_MSG_ALL=""
 
     for SRC_IP in "$@";
     do
         eval `printf "$RULE_PATTERN" -C $SRC_IP` 2> /dev/null
         # 0 - rule exists, 1 - rule not exist
         rule_exists=$?
-        [[ $rule_exists == 1 && $enabled == 1 ]] && eval `printf "$RULE_PATTERN" -A $SRC_IP` && SRC_IP_MSG="$SRC_IP_MSG$SRC_IP, "
-        [[ $rule_exists == 0 && $enabled == 0 ]] && eval `printf "$RULE_PATTERN" -D $SRC_IP` && SRC_IP_MSG="$SRC_IP_MSG$SRC_IP, "
-        SRC_IP_MSG_ALL="$SRC_IP_MSG_ALL$SRC_IP, "
+        [[ $rule_exists == 1 && $enabled == 1 ]] && eval `printf "$RULE_PATTERN" -A $SRC_IP`
+        [[ $rule_exists == 0 && $enabled == 0 ]] && eval `printf "$RULE_PATTERN" -D $SRC_IP`
+        SRC_IP_MSG="$SRC_IP_MSG$SRC_IP, "
     done
         
     REDIRECT_MSG="%s redirection to port $SVC_PORT for clients: %s"
-    [ -z "$SRC_IP_MSG" ] && REDIRECT_MSG="already done: $REDIRECT_MSG" && SRC_IP_MSG=$SRC_IP_MSG_ALL
+    #[ -z "$SRC_IP_MSG" ] && REDIRECT_MSG="already done: $REDIRECT_MSG" && SRC_IP_MSG=$SRC_IP_MSG_ALL
     ACTION_MSG=`[ $enabled == 1 ] && echo "enable" || echo "disable"`
     # remove last two chars
     SRC_IP_MSG=${SRC_IP_MSG%??}
